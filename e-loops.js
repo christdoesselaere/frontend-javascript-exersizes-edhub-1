6607418fdc91c25c19d91225911f09b676cce5bc
christdoesselaere <christ@doesselaere.nl>
2021-01-14 03:06:18 +0100

    answers e-loops.js

--- e-loops.js
+++ e-loops.js
@@ -1,7 +1,10 @@
 // ==========================================
 // 1. Maak een for-loop die 3 keer "Hoera!" in de console logt
 // ==========================================
-
+for (let answer1 = 0; answer1 < 3; answer1++) {
+    const answer1 = "Hoera!";
+    console.log(answer1);
+}
 
 // ==========================================
 // 2. Maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -18,7 +21,17 @@
 // >> 7
 // >> 9
 // ==========================================
-
+for (let answer2 = 0; answer2 < 10; answer2++) {
+    if (answer2 < 3) {
+        console.log(answer2);
+    }
+    if ((answer2 > 2) && (answer2 < 6)) {
+        console.log("> " + answer2);
+    }
+    if (answer2 > 5) {
+        console.log(">> " + answer2);
+    }
+}
 
 // ==========================================
 // 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -33,4 +46,11 @@
 // tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // hier bestaat een speciale operator voor
 // ==========================================
-
+for (let answer3 = 0; answer3 < 16; answer3++) {
+    if (answer3%2 === 0) {
+        console.log(answer3 + " is even");
+    }
+    if (answer3%2 !== 0) {
+        console.log(answer3 + " is oneven");
+    }
+}
